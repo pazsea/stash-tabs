1b70025656ed35f03ccb5b48373526866c9b8194
pazsea <patrick.sjoberg.se@gmail.com>
2021-04-01 21:34:57 +0200

    Add some style to list

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -18,10 +18,7 @@
       const { name, tabPaths }: IStashedItem = stash.value;
       switch (stash.type) {
         case "add-stash":
-          state = [
-            { name: name, tabPaths: tabPaths },
-            ...state,
-          ];
+          state = [{ name: name, tabPaths: tabPaths }, ...state];
           storeStashedItem(state);
           break;
       }
@@ -31,29 +28,23 @@
     }
   });
 
+  // const clear = () => {
+  //   state = [];
+  //   if (localStorage.getItem(storageKey)) localStorage.removeItem(storageKey);
+  // }
+
   const storeStashedItem = (state: IStashedItem[]) => {
     localStorage.setItem(storageKey, JSON.stringify(state));
-  }
+  };
 
   const handleClick = (item: IStashedItem) => {
-  console.log("🚀 ~ file: Sidebar.svelte ~ line 30 ~ handleClick ~ item", item)
+    console.log(
+      "🚀 ~ file: Sidebar.svelte ~ line 30 ~ handleClick ~ item",
+      item
+    );
     if (!item || !item.tabPaths) return;
     tsvscode.postMessage({ type: "onOpenTabs", value: item });
-  }
-
-  // onMount(() => {
-  //     window.addEventListener("stash", (event) => {
-  //         const message = event.data; // The json data that the extension sent
-  //         switch (message.type) {
-  //             case "new-todo":
-  //                 todos = [
-  //                     { text: message.value, isOpend: false },
-  //                     ...todos,
-  //                 ];
-  //                 break;
-  //         }
-  //     });
-  // });
+  };
 </script>
 
 <form
@@ -68,10 +59,13 @@
 
 <ul>
   {#each state as item (item.name)}
-    <li
-      on:click={() => handleClick(item)}
-    >
-      {item.name}
+    <li class:stashedItem={true} on:click={() => handleClick(item)}>
+      <div class:stashedItem_container={true}>
+        <div>
+          {item.name}
+        </div>
+        <div>X</div>
+      </div>
     </li>
   {/each}
 </ul>
@@ -95,6 +89,30 @@
         tsvscode.addTabs({ type: 'onError', value: 'ERROR MESSAGE' });
     }}>Click me for error</button> -->
 <style>
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  .stashedItem {
+    padding: 5px 10px;
+    margin: 5px 0;
+    border: 1px solid lightslategray;
+    background-color: #D9DBF1;
+    color: black;
+  }
+  .stashedItem_container {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+  }
+
+  .stashedItem:hover {
+    border: 1px solid lightblue;
+    color: white;
+    background: #0C7C59;
+  }
   .alwaysPop {
     background-color: green;
   }
